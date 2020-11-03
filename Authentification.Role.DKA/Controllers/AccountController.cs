fd25c9a4d35168208430d6accc3cc549f14bf99a
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 14:40:49 +0200

    Ajout des attributs dans le contrlleur de Compte (Account)

--- Authentification.Role.DKA/Controllers/AccountController.cs
+++ Authentification.Role.DKA/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Authentification.Role.DKA.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,15 @@ namespace Authentification.Role.DKA.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+        public AccountController(UserManager<ApplicationUser> userMgr,
+        SignInManager<ApplicationUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
