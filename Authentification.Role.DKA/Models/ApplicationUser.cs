f0964f093744acb52854de1c409dd6874ed08e99
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 11:03:44 +0200

    Ajout des constructeurs, un qui initialise le rolename

--- Authentification.Role.DKA/Models/ApplicationUser.cs
+++ Authentification.Role.DKA/Models/ApplicationUser.cs
@@ -23,6 +23,8 @@ namespace Authentification.Role.DKA.Models
 
     public class ApplicationRole : IdentityRole
     {
+        public ApplicationRole() { }
 
+        public ApplicationRole(string roleName) : base(roleName) { }
     }
 }
