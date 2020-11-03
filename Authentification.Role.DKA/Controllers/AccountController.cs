6f9f31c5cdd3287fcdd24fc1815d0d16467536be
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 14:39:20 +0200

    Création d'un nouveau dossier de vue avec ajout de la vue login

---
+++ Authentification.Role.DKA/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Authentification.Role.DKA.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentification.Role.DKA.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            return View(details);
+        }
+    }
+}
--- Authentification.Role.DKA/Controllers/HomeController.cs
+++ Authentification.Role.DKA/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Authentification.Role.DKA.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Authentification.Role.DKA.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ViewResult Index() => View(new Dictionary<string, object> { ["Placeholder"] = "Placehoder" });
