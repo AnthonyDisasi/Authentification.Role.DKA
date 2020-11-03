52326b3d0707f3d5bb063398da0f2fc91b57c0b1
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 10:54:17 +0200

    Add project files.

---
+++ Authentification.Role.DKA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Authentification.Role.DKA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
