eb1ca062f3ea29fe1773e4045180deff22547d63
AnthonyDisasi <anthonydisasi@gmail.com>
2020-11-03 12:01:40 +0200

    Implémentation du controlleur admin qui aura droit et privilege pour créer un utilisateur et création d'une vue index dans un nouveau dossier admin

---
+++ Authentification.Role.DKA/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentification.Role.DKA.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentification.Role.DKA.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<ApplicationUser> UserMana;
+
+        public AdminController(UserManager<ApplicationUser> _UserMana)
+        {
+            UserMana = _UserMana;
+        }
+        public ViewResult Index() => View(UserMana.Users);
+    }
+}
